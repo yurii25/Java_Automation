8c443e358705b8080d495f2ba1cc3562bcdd5c52
yurii25 <baitsaryura@gmail.com>
2022-12-18 22:54:53 +0200

    initial

--- src/chapter9/Ball.java
+++ src/chapter9/Ball.java
@@ -1,25 +1,22 @@
-/*
-5. Написать Enum “Ball” у него будут поля
-- размер (double)
-- вид спорты (String)
-- цена (double)
-- материал (String)
-добавить гетеры и конструктор
-добавить 5 видов мячей
-переопределить toString
-в классе BallTester c помощью toString вывести в консоль инфу о мячах
- */
 package chapter9;
 
-public enum Ball {
-    BALL1(30, " Football ", 50, " Leather"),
-    BALL2(25, " Volleyball ", 30, " Rubber"),
-    BALL3(8, " Basketball ", 35, " Leather"),
-    BALL4(10, " Pool ", 70, " Plastic"),
-    BALL5(5, " Golf ", 20, " Plastic");
+import java.util.Formatter;
 
-    private double size, price;
-    private String sport,material;
+public enum Ball {
+    BALL1(30, "Football", 50, "Leather"),
+    BALL2(25, "Volleyball", 30, "Rubber"),
+    BALL3(8, "Basketball", 35, "Leather"),
+    BALL4(10, "Pool", 70, "Plastic"),
+    BALL5(5, "Golf", 20, "Plastic");
+
+
+
+    private double size;
+    private String sport;
+    private double price;
+    private String material;
+
+
 
     private Ball( double ballSize, String typeOfSport, double ballPrice, String ballMaterial){
         size = ballSize;
@@ -30,7 +27,8 @@ public enum Ball {
     }
 
     public String getProperties(Ball ball){
-        return ball.size + ball.sport + ball.price + ball.material;
-    }
+
+        return "\t" + ball.size + " \t" + ball.sport + "    \t" + ball.price + "\t" + ball.material;
+   }
 
 }
--- src/chapter9/BallTester.java
+++ src/chapter9/BallTester.java
@@ -3,16 +3,30 @@ package chapter9;
 public class BallTester {
     public static void main(String[] args) {
 
+        // FIRST PRINT SOLUTION
+
+        System.out.println("NAME    SIZE    SPORT           PRICE   MATERIAL");
+
+        for (Ball ball: Ball.values()){
+            System.out.printf("%s%s%n", ball, ball.getProperties(ball));
+        }
+        System.out.println(" ");
+        System.out.println("OR: \n");
+
+        // SECOND PRINT SOLUTION
 
 
-        System.out.println(Ball.BALL1 + ":    " + Ball.BALL1.getProperties(Ball.BALL1));
-        System.out.println(Ball.BALL2 + ":    " + Ball.BALL2.getProperties(Ball.BALL2));
-        System.out.println(Ball.BALL3 + ":    " + Ball.BALL3.getProperties(Ball.BALL3));
-        System.out.println(Ball.BALL4 + ":    " + Ball.BALL4.getProperties(Ball.BALL4));
-        System.out.println(Ball.BALL5 + ":    " + Ball.BALL5.getProperties(Ball.BALL5));
+        System.out.println("NAME    SIZE    SPORT           PRICE   MATERIAL");
+        System.out.println(Ball.BALL1 + ":" + Ball.BALL1.getProperties(Ball.BALL1));
+        System.out.println(Ball.BALL2 + ":" + Ball.BALL2.getProperties(Ball.BALL2));
+        System.out.println(Ball.BALL3 + ":" + Ball.BALL3.getProperties(Ball.BALL3));
+        System.out.println(Ball.BALL4 + ":" + Ball.BALL4.getProperties(Ball.BALL4));
+        System.out.println(Ball.BALL5 + ":" + Ball.BALL5.getProperties(Ball.BALL5));
 
+        // не зміг відформатувати так, щоб два останні записи вписались в табличний вигляд
+        // Price та Material для BALL4 і BALL5 зміщені
 
-
-
+        // Також, я не створював геттерів - не впевнений чи вони тут взагалі потрібні,
+        // так як конструктор не може бути public
     }
 }
