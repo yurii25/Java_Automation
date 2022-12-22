17fea01b186cc7b9b4819b107b0e129d1e4900ca
yurii25 <baitsaryura@gmail.com>
2022-12-22 20:10:04 +0200

    initial

--- src/chapter10/Task7.java
+++ src/chapter10/Task7.java
@@ -32,7 +32,7 @@ public class Task7 {
         });
 
         System.out.println(macOS);
-        System.out.println(" workspace test7 ");
+        System.out.println(" workspace test8 ");
 
     }
 }
