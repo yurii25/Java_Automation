cee43436d5f9afd11ed142c390d92ad7dc5d643c
yurii25 <baitsaryura@gmail.com>
2022-12-26 20:35:57 +0200

    initial

--- src/chapter8/Task1.java
+++ src/chapter8/Task1.java
@@ -8,23 +8,41 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-        double sum = 0;
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Please, enter the number: ");
-            double a = scanner.nextDouble();
-                sum = sum + a;
-            System.out.println("The sum is: " + sum);
+//        double sum = 0;
+//
+//        try {
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println("Please, enter the number: ");
+//                double a = scanner.nextDouble();
+//                sum = sum + a;
+//            }
+//        }catch (InputMismatchException e){
+//            System.out.println("Invalid input type");
+//        }
+//
+//        System.out.println("The sum is: " + sum);
 
-            boolean InputMismatchException = false;
+            System.out.println("The sum is: " + xxx());
 
-            if(InputMismatchException){
-                throw new InputMismatchException();
-            }
-        }
-        System.out.println("The sum is: " + sum);
     }
 
+    public static double xxx(){
+        double sum = 0;
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Please, enter the number: ");
+                double a = scanner.nextDouble();
+                sum = sum + a;
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Invalid input type");
+        }
+        return sum;
+
+
+    }
 }
 
 
